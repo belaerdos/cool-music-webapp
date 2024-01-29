6df4b2f48b3a8e396c1256d95bffe2128dc7f4cd
belaerdos <erdosbelamusic@gmail.com>
2024-01-29 14:45:09 +0100

    Update CoolMusicRepository.java

--- cool-music/src/main/java/com/erdos/coolmusic/repositories/CoolMusicRepository.java
+++ cool-music/src/main/java/com/erdos/coolmusic/repositories/CoolMusicRepository.java
@@ -4,5 +4,7 @@ import com.erdos.coolmusic.models.CoolMusic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CoolMusicRepository extends JpaRepository<CoolMusic, Long> {}
